0ccbebb2f8dc295a66079f0c2e6f838319c3eaa9
CaptainAtos <b-che@gmx.de>
2026-01-23 20:01:43 +0100

    Init Commit

---
+++ VektorMathematik/VektorMathematik/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace VektorMathematik
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1, 2, 3);
+            Vector v2 = new Vector(3, 2, 1);
+
+            Vector Ergebnis = v1.Addition(v2);
+            Console.WriteLine($"Das Ergebnis ist Vector3: {Ergebnis.X}, {Ergebnis.Y}, {Ergebnis.Z}");
+
+            Ergebnis = v1.Subtract(v2);
+            Console.WriteLine($"Das Ergebnis ist Vector3: {Ergebnis.X},{Ergebnis.Y}, {Ergebnis.Z}");
+
+            Ergebnis = v1.Multiply(v2);
+            Console.WriteLine($"Das Ergebnis ist Vector3: {Ergebnis.X},{Ergebnis.Y}, {Ergebnis.Z}");
+
+            //Ergebnis = v1.Divide(v2);
+            //Console.WriteLine($"Das Ergebnis ist Vector3: {Ergebnis.X},{Ergebnis.Y}, {Ergebnis.Z}");
+
+        }
+    }
+}
---
+++ VektorMathematik/VektorMathematik/Vector.cs
@@ -0,0 +1,40 @@
+﻿namespace VektorMathematik
+{
+    partial class Program
+    {
+        class Vector
+        {
+            public float X = 0;
+            public float Y = 0;
+            public float Z = 0;
+
+            public Vector(float x, float y, float z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+            public Vector Addition(Vector oV)
+            {
+                return new Vector(X + oV.X, Y + oV.Y, Z + oV.Z);
+
+            }
+
+            public Vector Subtract(Vector oV) 
+            {
+                return new Vector(X - oV.X, Y - oV.Y, Z - oV.Z);
+            }
+
+            public Vector Multiply(Vector oV) 
+            {
+                return new Vector(X * 2, Y * 2, Z * 2);
+            
+            }
+
+            //public Vector Divide(Vector oV) 
+            //{
+            //    return new Vector(X / oV.X, Y / oV.Y, Z / oV.Z);
+            //}
+        }
+    }
+}
